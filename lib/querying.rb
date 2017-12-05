3106d5b8b6eb57cadc73e380cc2ea40aab1be46c
ronald vilorio <soccer23415@gmail.com>
2017-12-05 20:25:30 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -19,7 +19,7 @@ end
 def select_series_title_with_most_human_characters
   "SELECT series.title
   FROM series INNER JOIN characters ON series.id = characters.series_id
-  ORDER BY characters.species;"
+  GROUP BY characters.species ORDER BY characters.species;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
