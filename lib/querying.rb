f8dd067c4de5f86c1a6256095202900c1db456a6
ronald vilorio <soccer23415@gmail.com>
2017-12-05 19:55:25 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -11,7 +11,8 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT author.name,subgenre.name FROM authors INNER JOIN subgenre ON author.id = subgenre.id"
+  "SELECT author.name,subgenre.name 
+  FROM authors INNER JOIN subgenre ON author.id = subgenre.id"
 end
 
 def select_series_title_with_most_human_characters
