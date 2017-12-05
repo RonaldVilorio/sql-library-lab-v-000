23d68cf6e1ac5ac9b7c5de0828ef706af81a01c8
ronald vilorio <soccer23415@gmail.com>
2017-12-05 19:55:30 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -11,7 +11,7 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT author.name,subgenre.name 
+  "SELECT author.name,subgenre.name
   FROM authors INNER JOIN subgenre ON author.id = subgenre.id"
 end
 
