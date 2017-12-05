a2200f41acda6a67081dcd1c6dace355d07f2976
ronald vilorio <soccer23415@gmail.com>
2017-12-05 20:38:13 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -26,6 +26,6 @@ end
 def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name
   FROM characters INNER JOIN character_books ON characters.id = character_books.character_id
-  INNER JOIN books ON characters_books.book_id = books.id;
+  INNER JOIN books ON character_books.book_id = books.id;
   "
 end
